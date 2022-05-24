eb10e8bf4f0fcf8aab7eef6725b44b3987cf0873
Top1568 <apisit7985@gmail.com>
2022-05-24 21:42:56 +0700

    Update service

---
+++ services/TeacherService.js
@@ -0,0 +1,13 @@
+import studizAPI from "./AxiosConfig";
+
+const baseURL = process.env.baseURL;
+
+export default {
+  createClassroom(data) {
+    return studizAPI(baseURL).post('/create/classroom', data);
+  },
+
+  generatePinCode(classId) {
+    return studizAPI(baseURL).get(`/getPinCode/${classId})`);
+  }
+}
