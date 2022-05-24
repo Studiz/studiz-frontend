2a31d5d29e589c07e458e1888a6a354800b14283
jakkapongqcue <qjakkapong@gmail.com>
2022-05-24 21:16:10 +0700

    [Update] more, index

--- pages/index/more.vue
+++ pages/index/more.vue
@@ -2,21 +2,25 @@
   <div>
     <v-list color="transparent" rounded class="pa-0">
       <v-list-item to="profile" link class="px-3">
-        <v-list-item-avatar color="primary" size="36px" class="align-self-start align-self-md-center">
+        <v-list-item-avatar
+          color="primary"
+          size="36px"
+          class="align-self-start align-self-md-center"
+        >
           <v-icon large color="white">mdi-account-circle</v-icon>
           <!-- <v-img src="https://api.lorem.space/image/face?hash=92310" /> -->
         </v-list-item-avatar>
         <v-list-item-content>
           <v-row gutters>
             <v-col cols="12" md="6" class="d-flex items-center">
-              <v-list-item-title class="text-cap">user</v-list-item-title>
+              <v-list-item-title class="text-cap">{{displayName}}</v-list-item-title>
             </v-col>
             <v-spacer></v-spacer>
             <!-- hidden when loing -->
-            <v-col cols="6" md="2" class="align-self-center">
+            <v-col cols="6" md="2" class="align-self-center" v-if="!user">
               <v-btn to="signup" height="36" block color="secondary" outlined>sign up</v-btn>
             </v-col>
-            <v-col cols="6" md="2" class="align-self-center">
+            <v-col cols="6" md="2" class="align-self-center" v-if="!user">
               <v-btn to="login" height="36" block color="primary">login</v-btn>
             </v-col>
             <!-- hidden when loing -->
@@ -33,6 +37,7 @@
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    {{user}}
   </div>
 </template>
 
@@ -56,6 +61,18 @@ export default {
         // },
       ],
     }
+  },
+  computed: {
+    user() {
+      return this.$store.getters.user
+    },
+    displayName() {
+      return this.$store.getters.user
+        ? this.$store.getters.user.displayName
+          ? this.$store.getters.user.displayName
+          : `${this.$store.getters.user.firstName} ${this.$store.getters.user.lastName}`
+        : ''
+    },
   },
 }
 </script>
