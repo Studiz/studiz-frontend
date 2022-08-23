991c295a45debbb14e8fcf48a167a5ff05e5ce6f
jakkapongqcue <qjakkapong@gmail.com>
2022-08-23 15:05:32 +0700

    ♻️ refactor: row and col in class

--- pages/index/classroom/_classroomsid/index/quiz.vue
+++ pages/index/classroom/_classroomsid/index/quiz.vue
@@ -1,28 +1,26 @@
 <template>
   <div>
-    <v-row class="mt-2" no-gutters>
-      <v-col
-        cols="12"
-        md="4"
-        xl="3"
-        v-for="classroom in 10"
-        :key="classroom.name"
-        class="pa-md-1 py-1"
-      >
-        <v-card flat rounded="lg" class="bg-gradient-to-r from-cyan-500 to-blue-500 white--text">
+    <div class="grid grid-cols-1 gap-2 mt-2 lg:grid-cols-3">
+      <div v-for="classroom in 10" :key="classroom.name">
+        <v-card
+          flat
+          rounded="lg"
+          class="!border-0 bg-gradient-to-r from-cyan-500 to-blue-500 white--text w-full"
+        >
           <v-card-title class="w-full">
-            <div class="text-h5 w-10/12 truncate">
-              <span class="font-bold white--text">{{classroom.name}}</span>
+            <div class="w-10/12 truncate text-h5">
+              <span class="font-bold white--text">{{ classroom.name }}</span>
             </div>
           </v-card-title>
-
-          <v-card-subtitle
-            class="h-24 overflow-auto scrollbar white--text"
-          >{{classroom.description}}</v-card-subtitle>
-          <v-card-text class="flex justify-between items-start white--text">{{classroom.teachName}}</v-card-text>
+          <v-card-subtitle class="h-24 overflow-auto scrollbar white--text">{{
+            classroom.description
+          }}</v-card-subtitle>
+          <v-card-text class="flex items-start justify-between white--text">{{
+            classroom.teachName
+          }}</v-card-text>
         </v-card>
-      </v-col>
-    </v-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,5 +32,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
