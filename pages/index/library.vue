c80637ce5ef4baf684c8702673d9f1b920e0006f
jakkapongqcue <qjakkapong@gmail.com>
2022-08-24 17:15:01 +0700

    💄 style: UI library

--- pages/index/library.vue
+++ pages/index/library.vue
@@ -15,7 +15,7 @@
           >
             <v-container>
               <v-text-field
-                label="File name"
+                label="Quiz name"
                 outlined
                 required
                 :rules="rules.nameRules"
@@ -28,25 +28,52 @@
     </div>
     <v-divider class="my-5"></v-divider>
     <div class="grid grid-cols-1 gap-y-3">
-      <v-card
-        v-for="classroom in 10"
+      <div
+        v-for="classroom in 3"
         :key="classroom.name"
-        flat
-        rounded="lg"
-        class="!border-0 bg-gradient-to-r from-cyan-500 to-blue-500 white--text w-full"
+        class="w-full rounded-lg ring-1 background ring-black ring-opacity-10"
       >
         <v-card-title class="w-full">
-          <div class="w-10/12 truncate text-h5">
-            <span class="font-bold white--text">{{ classroom.name }}</span>
+          <div class="text-h5">
+            <span class="font-bold">Quiz name</span>
+          </div>
+          <v-spacer></v-spacer>
+          <div>
+            <v-btn icon center>
+              <v-icon> $vuetify.icons.edit </v-icon>
+            </v-btn>
+            <v-menu offset-y left transition="slide-y-transition">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon center v-bind="attrs" v-on="on">
+                  <v-icon class="w-full">mdi-dots-vertical </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="editItem(item)" class="gap-x-2">
+                  <v-icon>mdi-content-duplicate</v-icon>
+                  <v-list-item-title>Duplicate</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="Delete(item)" class="gap-x-2">
+                  <v-icon>mdi-delete</v-icon>
+                  <v-list-item-title>Delete</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </div>
         </v-card-title>
-        <v-card-subtitle class="h-24 overflow-auto scrollbar white--text">{{
-          classroom.description
-        }}</v-card-subtitle>
-        <v-card-text class="flex items-start justify-between white--text">{{
-          classroom.teachName
-        }}</v-card-text>
-      </v-card>
+        <!-- <v-card-subtitle class="h-24 overflow-auto scrollbar">
+          quiz description
+        </v-card-subtitle> -->
+        <v-card-text class="flex items-center justify-between">
+          <!-- <div class="px-3 py-1 rounded-full">10 items</div> -->
+          <!-- <v-chip color="secondary" class="!hover:bg-secondary">10 items</v-chip> -->
+          <span>edit 00/00/00</span>
+          <div>
+            <v-btn color="white" disabled>Assign</v-btn>
+            <v-btn color="primary">Start</v-btn>
+          </div>
+        </v-card-text>
+      </div>
     </div>
   </div>
 </template>
@@ -78,7 +105,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-::v-deep .btn-dialog-style {
+:v-deep(.btn-dialog-style) {
   @apply w-full md:w-auto;
 }
 </style>
