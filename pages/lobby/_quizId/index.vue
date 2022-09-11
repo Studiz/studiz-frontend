119c7811697533af7c1065b58015e227e9b6ccd6
Top1568 <apisit7985@gmail.com>
2022-09-11 22:33:36 +0700

    Update mock lobby

--- pages/lobby/_quizId/index.vue
+++ pages/lobby/_quizId/index.vue
@@ -103,7 +103,23 @@ export default {
   components: { LayoutQuiz },
   data() {
     return {
-      mockUser: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+      mockUser: [
+        'Student 1',
+        'Student 2',
+        'Student 3',
+        'Student 4',
+        'Student 5',
+        'Student 6',
+        'Student 7',
+        'Student 8',
+        'Student 9',
+        'Student 10',
+        'Student 11',
+        'Student 12',
+        'Student 13',
+        'Student 14',
+        'Student 15',
+      ],
       quizData: {},
     }
   },
