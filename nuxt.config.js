cbba98ac232a0b811db659f7a51cd9f3d8e4f031
jakkapongqcue <qjakkapong@gmail.com>
2022-09-15 23:52:54 +0700

    🔧 chore: remove pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -124,24 +124,24 @@ export default {
     host: '0.0.0.0',
     timing: false,
   },
-  pwa: {
-    icon: {
-      fileName: 'icon.png',
-      sizes: [64, 120, 144, 152, 192, 384, 512],
-      purpose: 'any',
-    },
-    manifest: {
-      icon: {
-        fileName: 'icon.png',
-        sizes: [64, 120, 144, 152, 192, 384, 512],
-      },
-      name: 'Studiz',
-      short_name: 'studiz',
-      lang: 'en',
-      theme_color: '#0c8ce9',
-      description:
-        'This web application will help students interact with friends and teachers to create an active learning process and make students feel more interested in learning and having fun.',
-      start_url: '/join',
-    },
-  },
+  // pwa: {
+  //   icon: {
+  //     fileName: 'icon.png',
+  //     sizes: [64, 120, 144, 152, 192, 384, 512],
+  //     purpose: 'any',
+  //   },
+  //   manifest: {
+  //     icon: {
+  //       fileName: 'icon.png',
+  //       sizes: [64, 120, 144, 152, 192, 384, 512],
+  //     },
+  //     name: 'Studiz',
+  //     short_name: 'studiz',
+  //     lang: 'en',
+  //     theme_color: '#0c8ce9',
+  //     description:
+  //       'This web application will help students interact with friends and teachers to create an active learning process and make students feel more interested in learning and having fun.',
+  //     start_url: '/join',
+  //   },
+  // },
 }
