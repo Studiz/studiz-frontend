14f296abff04cbcec65f1eff8b82a68507c8e58c
jakkapongqcue <qjakkapong@gmail.com>
2022-09-28 16:32:17 +0700

    ✨ feat:  implemen Progressive Web Application

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    // '@nuxtjs/pwa',
+    '@nuxtjs/pwa',
     '@nuxtjs/axios',
     [
       '@nuxtjs/firebase',
@@ -124,24 +124,24 @@ export default {
     host: '0.0.0.0',
     timing: false,
   },
-  // pwa: {
-  //   icon: {
-  //     fileName: 'icon.png',
-  //     sizes: [64, 120, 144, 152, 192, 384, 512],
-  //     purpose: 'any',
-  //   },
-  //   manifest: {
-  //     icon: {
-  //       fileName: 'icon.png',
-  //       sizes: [64, 120, 144, 152, 192, 384, 512],
-  //     },
-  //     name: 'Studiz',
-  //     short_name: 'studiz',
-  //     lang: 'en',
-  //     theme_color: '#0c8ce9',
-  //     description:
-  //       'This web application will help students interact with friends and teachers to create an active learning process and make students feel more interested in learning and having fun.',
-  //     start_url: '/join',
-  //   },
-  // },
+  pwa: {
+    icon: {
+      fileName: 'icon.png',
+      sizes: [64, 120, 144, 152, 192, 384, 512],
+      purpose: 'any',
+    },
+    manifest: {
+      icon: {
+        fileName: 'icon.png',
+        sizes: [64, 120, 144, 152, 192, 384, 512],
+      },
+      name: 'Studiz',
+      short_name: 'studiz',
+      lang: 'en',
+      theme_color: '#0c8ce9',
+      description:
+        'This web application will help students interact with friends and teachers to create an active learning process and make students feel more interested in learning and having fun.',
+      start_url: '/join',
+    },
+  },
 }
