53441c60a208627a26353320bd656b27030d8946
Top1568 <apisit7985@gmail.com>
2022-10-01 10:28:56 +0700

    Update reverse poxy for use socket

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 // const socket = io('http://localhost:9091/')
-const socket = io(process.env.webSocket)
-// const socket = io('https://www.studiz.games/studiz')
+// const socket = io(process.env.webSocket)
+const socket = io('https://www.studiz.games/studiz')
 
 export default socket
