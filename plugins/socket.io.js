4a2a4838990dc200acd647430a557560a3523e41
Top1568 <apisit7985@gmail.com>
2022-10-01 09:23:41 +0700

    Update env for socket

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client'
 
 // const socket = io('http://localhost:9091/')
-const socket = io(process.env.webSocket)
+// const socket = io(process.env.webSocket)
+const socket = io('https://www.studiz.games/studiz')
 
 export default socket
