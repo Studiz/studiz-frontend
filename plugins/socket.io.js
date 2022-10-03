aeb4c585ca696774941479fd82082e8ea307cb24
Top1568 <apisit7985@gmail.com>
2022-10-03 15:54:50 +0700

    Update reverse poxy and env

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 // const socket = io('http://localhost:9091/')
-const socket = io('http://20.239.140.141:5003/wsapp')
+const socket = io('https://www.studiz.games:5003/wsapp')
 // const socket = io(process.env.webSocket)
 // const socket = io('https://www.studiz.games/studiz')
 
