32dba5f58c786d17bb3a6df2eff3e373d451ed92
jakkapongqcue <qjakkapong@gmail.com>
2022-10-11 12:39:53 +0700

    💄 style: question typology

--- components/quiz/BaseQuestionText.vue
+++ components/quiz/BaseQuestionText.vue
@@ -22,15 +22,15 @@ export default {
     renderFontSize() {
       let length = this.renderQuestion.length
       if (length > 101) {
-        return 'md:text-4xl'
+        return 'md:text-2xl lg:text-3xl xl:text-4xl'
       } else if (length > 100) {
-        return 'md:text-5xl'
+        return 'md:text-3xl lg:text-4xl xl:text-5xl'
       } else if (length > 50) {
-        return 'md:text-6xl'
+        return 'md:text-4xl lg:text-5xl xl:text-6xl'
       } else if (length > 25) {
-        return 'md:text-7xl'
+        return 'md:text-5xl lg:text-6xl xl:text-7xl'
       } else {
-        return 'md:text-8xl'
+        return 'md:text-6xl lg:text-7xl xl:text-8xl'
       }
     },
   },
