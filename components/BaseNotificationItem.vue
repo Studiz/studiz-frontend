6c0d996eef5096f86424a05094c5117b151967a2
jakkapongqcue <qjakkapong@gmail.com>
2022-10-21 21:26:36 +0700

    🔧 chore: removee button tag

--- components/BaseNotificationItem.vue
+++ components/BaseNotificationItem.vue
@@ -1,58 +1,53 @@
 <template>
-  <button
-    class="focus:outline-none overflow-hidden w-full"
-    :class="expanded ? '' : 'cursor-pointer'"
+  <v-alert
     @click="openAction"
+    class="!p-3 md:!p-4"
+    color="primary"
+    icon="$vuetify.icons.quiz"
+    :prominent="expanded"
+    dark
   >
-    <v-alert
-      class="!p-3 md:!p-4"
-      color="primary"
-      icon="$vuetify.icons.quiz"
-      :prominent="expanded"
-      dark
-    >
-      <div class="flex gap-2" :class="expanded ? 'flex-row' : 'flex-col'">
-        <div class="flex flex-col items-start justify-center text-left">
-          <div class="inline-flex items-center gap-x-2 truncate">
-            <span class="sm:text-xl">{{ title }}</span>
-            <span class="w-1 h-1 bg-white/70 rounded-full" />
-            <base-time-to-text
-              textClass="text-sm"
-              :time="time"
-              :showTooltip="false"
-            />
-          </div>
-          <span
-            class="text-xs sm:text-base"
-            :class="[expanded ? 'line-clamp-2' : 'line-clamp-3']"
-          >
-            {{ content }}
-          </span>
-        </div>
-        <div class="flex">
-          <v-img
-            contain
-            class="self-center justify-self-center"
-            :class="[expanded ? 'w-14 h-14 sm:w-20 sm:h-20' : 'w-20 max-h-32']"
-            :src="image"
+    <div class="flex gap-2" :class="expanded ? 'flex-row' : 'flex-col'">
+      <div class="flex flex-col items-start justify-center text-left">
+        <div class="inline-flex items-center gap-x-2 truncate">
+          <span class="sm:text-xl">{{ title }}</span>
+          <span class="w-1 h-1 bg-white/70 rounded-full" />
+          <base-time-to-text
+            textClass="text-sm"
+            :time="time"
+            :showTooltip="false"
           />
         </div>
-
-        <button
-          @click.stop="expanded = !expanded"
-          @blur="expanded = true"
-          class="absolute right-3 transition-all flex justify-center items-center focus:outline-none w-7 h-7 p-0.5 bg-black/50 rounded-full"
-          :class="[expanded ? 'top-6 sm:top-10' : 'top-3 rotate-180']"
+        <span
+          class="text-xs sm:text-base"
+          :class="[expanded ? 'line-clamp-2' : 'line-clamp-3']"
         >
-          <v-icon dark>mdi-chevron-down</v-icon>
-        </button>
-
-        <div class="w-7">
-          <div class="w-7" />
-        </div>
+          {{ content }}
+        </span>
       </div>
-    </v-alert>
-  </button>
+      <div class="inline-flex">
+        <v-img
+          contain
+          class="self-center justify-self-center"
+          :class="[expanded ? 'w-14 h-14 sm:w-20 sm:h-20' : 'w-20 max-h-32']"
+          :src="image"
+        />
+      </div>
+
+      <button
+        @click.stop="expanded = !expanded"
+        @blur="expanded = true"
+        class="absolute right-3 transition-all flex justify-center items-center focus:outline-none w-7 h-7 p-0.5 bg-black/50 rounded-full"
+        :class="[expanded ? 'top-6 sm:top-10' : 'top-3 rotate-180']"
+      >
+        <v-icon dark>mdi-chevron-down</v-icon>
+      </button>
+
+      <div class="w-7">
+        <div class="w-7" />
+      </div>
+    </div>
+  </v-alert>
 </template>
 
 <script>
