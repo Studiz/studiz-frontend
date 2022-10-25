f05c48fb83117e07f476865a3f9c7b3ebd01a225
jakkapongqcue <qjakkapong@gmail.com>
2022-10-25 18:24:12 +0700

    💄 style: number of notifications

---
+++ plugins/notification-number.js
@@ -0,0 +1,40 @@
+export default {
+  watch: {
+    notifications() {
+      this.notificationNumber()
+    },
+  },
+  methods: {
+    notificationNumber() {
+      let notiNumber
+      notiNumber = this.$store.getters.notifications
+        ? this.$store.getters.notifications.length
+        : ''
+
+      let listNoti = document.getElementById('list-Notification')
+      listNoti.innerHTML = notiNumber
+      let bottonNoti = document.getElementById('bottom-nav-Notification')
+      bottonNoti.innerHTML = notiNumber
+
+      if (this.$store.getters.notifications.length > 0) {
+        listNoti.classList.add('Notification')
+        bottonNoti.classList.add('Notification')
+      } else {
+        listNoti.classList.remove('Notification')
+        bottonNoti.classList.remove('Notification')
+        bottonNoti.classList.add('hidden')
+        listNoti.classList.add('hidden')
+      }
+    },
+  },
+  computed: {
+    notifications() {
+      return this.$store.getters.notifications
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.notificationNumber()
+    }, 500)
+  },
+}
