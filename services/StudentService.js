cc8ef249cf0f858db5aa416d37d1adceef2ba2d4
Top1568 <apisit7985@gmail.com>
2022-11-04 18:22:49 +0700

    Update api random item

--- services/StudentService.js
+++ services/StudentService.js
@@ -1,55 +1,69 @@
-import studizAPI from "./AxiosConfig";
+import studizAPI from './AxiosConfig'
 
-const baseURL = process.env.baseURL;
+const baseURL = process.env.baseURL
 
 export default {
   joinClassroom(pinCode, studentId) {
-    return studizAPI(baseURL).put(`/join/classroom/${pinCode}/${studentId}`, void 0, {
-      headers: {
-        'token': 'Bearer ' + localStorage.getItem('accessToken')
+    return studizAPI(baseURL).put(
+      `/join/classroom/${pinCode}/${studentId}`,
+      void 0,
+      {
+        headers: {
+          token: 'Bearer ' + localStorage.getItem('accessToken'),
+        },
       }
-    });
+    )
   },
 
   leaveClassroom(classroomId, studentId) {
-    return studizAPI(baseURL).delete(`/leftClassroom/${classroomId}/${studentId}`, {
-      headers: {
-        'token': 'Bearer ' + localStorage.getItem('accessToken')
+    return studizAPI(baseURL).delete(
+      `/leftClassroom/${classroomId}/${studentId}`,
+      {
+        headers: {
+          token: 'Bearer ' + localStorage.getItem('accessToken'),
+        },
       }
-    });
+    )
   },
 
   updateProfile(studentId, student) {
     return studizAPI(baseURL).put(`/update/student/${studentId}`, student, {
       headers: {
-        'token': 'Bearer ' + localStorage.getItem('accessToken')
-      }
-    });
+        token: 'Bearer ' + localStorage.getItem('accessToken'),
+      },
+    })
   },
 
   updateImageProfile(studentId, image) {
-    return studizAPI(baseURL).post(`/upload/student/image/${studentId}`, image, {
-      headers: {
-        'token': 'Bearer ' + localStorage.getItem('accessToken')
+    return studizAPI(baseURL).post(
+      `/upload/student/image/${studentId}`,
+      image,
+      {
+        headers: {
+          token: 'Bearer ' + localStorage.getItem('accessToken'),
+        },
       }
-    });
+    )
   },
 
   joinQuiz(pinCode, studentId) {
     // return studizAPI(baseURL).patch(`/joinQuiz/${pinCode}`, studentId);
-    return studizAPI(baseURL).post(`/joinQuiz/${pinCode}`);
+    return studizAPI(baseURL).post(`/joinQuiz/${pinCode}`)
   },
 
   getQuizById(quizId) {
-    return studizAPI(baseURL).get(`/get/quizForStudent/${quizId}`);
+    return studizAPI(baseURL).get(`/get/quizForStudent/${quizId}`)
   },
 
   getQuizHistoryByStudentUid(uid) {
     return studizAPI(baseURL).get(`/get/quizHistory/studentUid/${uid}`, {
       headers: {
-        'token': 'Bearer ' + localStorage.getItem('accessToken')
-      }
-    });
-  }
+        token: 'Bearer ' + localStorage.getItem('accessToken'),
+      },
+    })
+  },
 
+  randomItems(numberItems) {
+    return studizAPI(baseURL).get(`/randomItems/${numberItems}`)
+  },
 }
