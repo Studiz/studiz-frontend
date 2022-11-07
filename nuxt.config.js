975fa19af1094725c86e9d18800c6d6d84720b54
jakkapongqcue <qjakkapong@gmail.com>
2022-11-07 21:58:36 +0700

    ✅ test: nuxt config for pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -145,8 +145,8 @@ export default {
         'This web application will help students interact with friends and teachers to create an active learning process and make students feel more interested in learning and having fun.',
       id: '/join',
       start_url: '/join',
-      background_color: '#0c8ce9',
-      scope: '/',
+      background_color: '#22A1EE',
+      scope: '/join',
     },
   },
 }
