0845683f018d69d77be41bd63c67fffde6adb5cb
qjakkapong@gmail.com <qjakkapong@gmail.com>
2023-06-21 23:28:43 +0700

    re

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,8 +104,8 @@ export default {
   env: {
     // baseURL: process.env.BASE_URL,
     // webSocket: process.env.WEB_SOCKET_URL,
-    baseURL: 'http://20.2.82.170:9091/api/',
-    webSocket: 'http://20.2.82.170:9091/',
+    baseURL: '20.2.82.170:9091/api',
+    webSocket: '20.2.82.170:9091/',
   },
 
   router: {
