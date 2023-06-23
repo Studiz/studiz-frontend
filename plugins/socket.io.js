d317ff90231a533573e0b5b598e2109125c978d8
qjakkapong@gmail.com <qjakkapong@gmail.com>
2023-06-23 16:17:25 +0700

    test 1

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 // const socket = io('http://localhost:9091/')
-const socket = io('https://www.studiz.games/', {
+const socket = io('http://20.2.82.170/', {
   secure: true,
   rejectUnauthorized: false,
   path: "/socket.io/",
